fe3990f20bcabe86a6cc74908804f199ff774930
serge.monnerat <serge.monnerat@NB19-B0I-SMO.intra.eiaj.ch>
2024-03-07 14:01:59 +0100

    saaS

--- source/essaigit.c
+++ source/essaigit.c
@@ -63,10 +63,7 @@ int main(void) {
     volatile static int i = 0 ;
     /* Enter an infinite loop, just incrementing a counter. */
     while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
+
     }
     return 0 ;
 }
